75e882ef17e1c9c8455f7cd10b5c66c45195be92
Wojciech Frącz <fraczwojciech@gmail.com>
2023-03-02 13:45:23 +0100

    Add checkstyle configuration

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -7,14 +7,17 @@ import com.indvd00m.ascii.render.api.IRender;
 import com.indvd00m.ascii.render.elements.PseudoText;
 
 /**
- * Simple main application
+ * Simple main application.
  */
 public class App {
+    public static final int RANDOM_NUMBER = 4;
+    public static final int TEXT_WIDTH = 120;
+    public static final int TEXT_HEIGHT = 20;
 
     public static void main(String[] args) {
         IRender render = new Render();
         IContextBuilder builder = render.newBuilder();
-        builder.width(120).height(20);
+        builder.width(TEXT_WIDTH).height(TEXT_HEIGHT);
         builder.element(new PseudoText("Hello Maven!"));
         ICanvas canvas = render.render(builder.build());
         String s = canvas.getText();
@@ -24,6 +27,6 @@ public class App {
     public int getRandomNumber() {
         // chosen by fair dice roll.
         // guaranteed to be random
-        return 4;
+        return RANDOM_NUMBER;
     }
 }
